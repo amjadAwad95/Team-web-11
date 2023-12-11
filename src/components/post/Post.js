cef1896b11138d117a1c5f104b55fed4a3e13120
amjad awad <amjadawad129@gmail.com>
2023-12-11 16:25:24 +0200

    styling a magrgin fot the post

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -3,41 +3,45 @@ import PostAction from "../post_action/PostAction";
 const Post = (props) => {
     return (
         <>
-            <div className="container" >
-                <section className="mx-auto my-5" style={{ maxWidth: '50rem' }}>
-                    <div className="card" style={{ borderRadius: "0", padding: "20px" }}>
-                        <div className="card-head d-flex">
-                            <img src={props.profileImage} className="rounded-circle me-3"
-                                style={{ width: "40px", height: "40px" }}
-                            />
-                            <div style={{ lineHeight: "0" }}>
-                                <h5 style={{ float: "left", clear: "both" }}>{props.name}
-                                    <span style={{ color: "#a29b9e", fontSize: "15px", paddingLeft: "10px" }}> . {props.time}</span></h5>
-                                <p style={{ float: "left", clear: "both", color: "#a29b9e" }}>{props.desc}</p>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            <p style={{ textAlign: "left", color: "#a29b9e" }}>
-                                {props.postText}
-                            </p>
-                            <div className="bg-image">
-                                <img className="img-fluid" src={props.image} style={{ borderRadius: "7px" }} />
-                            </div>
-                        </div>
-                        <div className="card-footer bg-white" style={{ border: "0" }}>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="d-flex align-items-center">
-                                    <PostAction color="#677e92" icon="bi bi-hand-thumbs-up-fill" action="Like" counter={props.like} id={props.id} />
-                                    <a href="#!" className="btn" style={{ color: "#677e92 " }}>
-                                        <i className="bi bi-chat-fill" style={{ paddingRight: "5px" }}></i>
-                                        Comments({props.numberComment})
-                                    </a>
+            <div className="container">
+                <div className="row justify-content-start">
+                    <div className="col-md-6" style={{ marginBottom: "-40px", marginLeft: "-40px" }}>
+                        <section className="mx-auto my-5 mb-1" style={{ maxWidth: '50rem' }}>
+                            <div className="card" style={{ borderRadius: "0", padding: "20px" }}>
+                                <div className="card-head d-flex">
+                                    <img src={props.profileImage} className="rounded-circle me-3"
+                                        style={{ width: "40px", height: "40px" }}
+                                    />
+                                    <div style={{ lineHeight: "0" }}>
+                                        <h5 style={{ float: "left", clear: "both" }}>{props.name}
+                                            <span style={{ color: "#a29b9e", fontSize: "15px", paddingLeft: "10px" }}> . {props.time}</span></h5>
+                                        <p style={{ float: "left", clear: "both", color: "#a29b9e" }}>{props.desc}</p>
+                                    </div>
                                 </div>
-                                <PostAction color="#677e92" icon="bi bi-share-fill" action="Share" counter={props.share} id={props.id} />
+                                <div className="card-body">
+                                    <p style={{ textAlign: "left", color: "#a29b9e" }}>
+                                        {props.postText}
+                                    </p>
+                                    <div className="bg-image">
+                                        <img className="img-fluid" src={props.image} style={{ borderRadius: "7px" }} />
+                                    </div>
+                                </div>
+                                <div className="card-footer bg-white" style={{ border: "0" }}>
+                                    <div className="d-flex justify-content-between align-items-center">
+                                        <div className="d-flex align-items-center">
+                                            <PostAction color="#677e92" icon="bi bi-hand-thumbs-up-fill" action="Like" counter={props.like} id={props.id} />
+                                            <a href="#!" className="btn" style={{ color: "#677e92 " }}>
+                                                <i className="bi bi-chat-fill" style={{ paddingRight: "5px" }}></i>
+                                                Comments({props.numberComment})
+                                            </a>
+                                        </div>
+                                        <PostAction color="#677e92" icon="bi bi-share-fill" action="Share" counter={props.share} id={props.id} />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </section>
                     </div>
-                </section>
+                </div>
             </div>
         </>
     )
