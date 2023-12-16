e7d27601f4fb16c3aabb19aa850c580818cb601e
Ahmad <swesee2021@gmail.com>
2023-12-17 00:49:37 +0200

    add about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Posts from './components/Posts';
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import About from "./About";
+
 
 function App() {
   return (
     <div className="App">
+      <About />
       <Posts />
+      
     </div>
   );
 }
