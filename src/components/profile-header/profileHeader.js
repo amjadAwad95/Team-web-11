c8d5cfbbae599e6b3dc6fff338510e2998d1ddbe
amjad awad <amjadawad129@gmail.com>
2023-12-21 22:04:44 +0200

    some edit

--- src/components/profile-header/profileHeader.js
+++ src/components/profile-header/profileHeader.js
@@ -9,82 +9,86 @@ const ProfileHeader = () => {
   console.log(data)
 
 
-  return (
-    <div className="card">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${userData.coverPhotoUrl})` }}
-      ></div>
-      <div className="card-body py-0">
-        <div className="d-sm-flex align-items-start text-center text-sm-start">
-          <div>
-            <div className="avatar avatar-xxl mt-n5 mb-3" style={{ marginTop: "-60px" }}>
-              <img
-                className="avatar-img rounded-circle border border-white border-3"
-                src={userData.profilePictureUrl}
-                alt=""
-                style={{ width: "150px", height: "150px" }}
-              />
+  return data.map((headerData) => {
+    return (
+      <div className="card" key={headerData.id}>
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${headerData.coverPhotoUrl})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
+        ></div>
+        <div className="card-body py-0">
+          <div className="d-sm-flex align-items-start text-center text-sm-start">
+            <div>
+              <div className="avatar avatar-xxl mt-n5 mb-3" style={{ marginTop: "-60px" }}>
+                <img
+                  className="avatar-img rounded-circle border border-white border-3"
+                  src={headerData.profilePictureUrl}
+                  alt=""
+                  style={{ width: "150px", height: "150px" }}
+                />
+              </div>
             </div>
-          </div>
-          <div className="ms-sm-4 mt-sm-3">
-            <h1 className="mb-0 h5">
-              {userData.username}{" "}
-              {userData.isVerified && (
-                <i className="bi bi-patch-check-fill text-success small" />
-              )}
-            </h1>
-            <p>{userData.connections} connections</p>
-          </div>
-          <div className="d-flex mt-3 justify-content-center ms-sm-auto">
-            <button
-              className="btn btn-danger-soft me-2"
-              type="button"
-              style={{ background: "#fae8eb", color: "#dc3545" }}
-            >
-              <i className="bi bi-pencil-fill pe-1" /> Edit profile
-            </button>
-            <div className="dropdown">
+            <div className="ms-sm-4 mt-sm-3">
+              <h1 className="mb-0 h5">
+                {headerData.userName}{" "}
+                {headerData.isVerified && (
+                  <i className="bi bi-patch-check-fill text-success small" />
+                )}
+              </h1>
+              <p>{headerData.connections} connections</p>
+            </div>
+            <div className="d-flex mt-3 justify-content-center ms-sm-auto">
               <button
-                className="icon-md btn btn-light"
+                className="btn btn-danger-soft me-2"
                 type="button"
-                id="profileAction2"
-                aria-expanded="false"
+                style={{ background: "#fae8eb", color: "#dc3545" }}
               >
-                <i className="bi bi-three-dots" />
+                <i className="bi bi-pencil-fill pe-1" /> {headerData.editButton}
               </button>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="profileAction2"
-              >
+              <div className="dropdown">
+                <button
+                  className="icon-md btn btn-light"
+                  type="button"
+                  id="profileAction2"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="bi bi-three-dots" />
+                </button>
+                <ul
+                  className="dropdown-menu dropdown-menu-end"
+                  aria-labelledby="profileAction2"
+                >
 
-              </ul>
+                  <li><a className="dropdown-item" href="#"> <i class="bi bi-lock fa-fw pe-2"></i>{headerData.loackProfile}</a></li>
+                  <li> <a className="dropdown-item" href="#"> <i class="bi bi-gear fa-fw pe-2"></i>{headerData.settingsButton}</a></li>
+                </ul>
+              </div>
             </div>
           </div>
+          <ul className="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0 ">
+            <li className="list-inline-item">
+              <i className="bi bi-briefcase me-1" /> {headerData.work}
+            </li>
+            <li className="list-inline-item">
+              <i className="bi bi-geo-alt me-1" /> {headerData.location}
+            </li>
+            <li className="list-inline-item">
+              <i className="bi bi-calendar2-plus me-1" /> Joined on {headerData.joinedDate}
+            </li>
+          </ul>
         </div>
-        <ul className="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0 ">
-          <li className="list-inline-item">
-            <i className="bi bi-briefcase me-1" /> {userData.work}
-          </li>
-          <li className="list-inline-item">
-            <i className="bi bi-geo-alt me-1" /> {userData.location}
-          </li>
-          <li className="list-inline-item">
-            <i className="bi bi-calendar2-plus me-1" /> Joined on {userData.joinedDate}
-          </li>
-        </ul>
-      </div>
-      <div className="card-footer mt-3 pt-2 pb-0 mx-3">
-        <ul className="nav nav-bottom-line align-items-center justify-content-center justify-content-md-start mb-0 border-0">
-          <li className="nav-item">
-            <a
-              className={`nav-link ${active === "Posts" ? "active" : "non-active"}`}
-              onClick={() => setActive("Posts")}
-            >
-              Posts
-            </a>
-          </li>
-          <li><a className={active == "About" ? "active" : "non-active"} onClick={() => { setActive("About") }}>About</a></li>
+        <div className="card-footer mt-3 pt-2 pb-0 mx-3">
+          <ul className="nav nav-bottom-line align-items-center justify-content-center justify-content-md-start mb-0 border-0">
+            <li className="nav-item">
+              <a
+                className={`nav-link ${active === "Posts" ? "active" : "non-active"}`}
+                onClick={() => setActive("Posts")}
+              >
+                {headerData.postButton}
+              </a>
+            </li>
+            <li><a className={active == "About" ? "active" : "non-active"} onClick={() => { setActive("About") }}>{headerData.aboutButton}</a></li>
 
             <li><a className={active == "Connections" ? "active" : "non-active"} onClick={() => { setActive("Connections") }}>{headerData.connectionButton}
               <span className="connection-count">{headerData.connections}</span></a></li>
@@ -99,3 +103,4 @@ const ProfileHeader = () => {
   });
 };
 export default ProfileHeader;
+
